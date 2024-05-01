965d11b0b918d05685cc940a732a588c59125f59
Samir-Zade <samirzadeofficial@gmail.com>
2024-05-01 19:29:59 +0530

    Files Added

---
+++ src/CementProject/utils.py
@@ -0,0 +1,72 @@
+import os, sys
+from src.CementProject.exception import CustomException
+from src.CementProject.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+import pickle
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading Sql database started")
+    try:
+        mydb = pymysql.connect(
+            host = host,
+            user = user,
+            password = password,
+            db = db
+        )
+        logging.info("connection established", mydb)
+        df = pd.read_sql_query("Select * from students", mydb)
+        print(df.head())
+        return df
+
+    except Exception as ex:
+        raise CustomException(ex)
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok= True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv = 3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train_pred, y_train)
+            test_model_score = r2_score(y_test_pred, y_test)
+
+            report[list(models.keys())[i]] = test_model_score
+            return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
--- template.py
+++ template.py
@@ -19,6 +19,7 @@ list_of_files = [
     f"src/{project_name}/exception.py",
     f"src/{project_name}/logger.py",
     f"src/{project_name}/utils.py",
+    "notebook/data",
     "app.py",
     "Dockerfile",
     "requirements.txt",
